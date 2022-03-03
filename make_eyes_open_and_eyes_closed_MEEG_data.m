b791e603ef4a337c2ab48481b391e203c5fbf829
Frederikvdsteen <35363636+Frederikvdsteen@users.noreply.github.com>
2022-03-03 08:39:50 +0100

    Update make_eyes_open_and_eyes_closed_MEEG_data.m

--- make_eyes_open_and_eyes_closed_MEEG_data.m
+++ make_eyes_open_and_eyes_closed_MEEG_data.m
@@ -1,9 +1,9 @@
 %%%% this script converts preprocessed EEGLAB data into SPM12 MEEG objacts
 %%% with both EO (last 10secs) and EC (first 10secs) data stored in a single MEEG object
 %%%% files for EO and EC EEGLAB cleaned data
-fold = dir(fullfile(['/home/frederik/data/EEG_rest/prep_cleaned/*R01*cleaned*.set']))
+fold = dir(fullfile(['/Users/fvdsteen/data/EEG_rest/prep_cleaned/*R01*cleaned*.set']))
 files for EC data
-fold2 = dir(fullfile(['/home/frederik/data/EEG_rest/prep_cleaned/*R02*cleaned*.set']))
+fold2 = dir(fullfile(['/Users/fvdsteen/data/EEG_rest/prep_cleaned/*R02*cleaned*.set']))
 
 STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[];
 
@@ -13,15 +13,15 @@ tmp = [];
 for i = 1:length(fold);
 STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[];
 %%%% load pre-processed EEGLAB data EO
-EEG = pop_loadset('filename',fold(i).name,'filepath','/home/frederik/data/EEG_rest/prep_cleaned/');
+EEG = pop_loadset('filename',fold(i).name,'filepath','/Users/fvdsteen/data/EEG_rest/prep_cleaned/');
 %%% load pre-processed EEGLAB data EC
-EEG2 = pop_loadset('filename',fold2(i).name,'filepath','/home/frederik/data/EEG_rest/prep_cleaned/');
+EEG2 = pop_loadset('filename',fold2(i).name,'filepath','/Users/fvdsteen/data/EEG_rest/prep_cleaned/');
 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
 
 %% load template MEEG object (made available at github)
-D = spm_eeg_load('/home/frederik/data/EEG_rest/spm_files/test5');
+D = spm_eeg_load('/Users/fvdsteen/data/EEG_rest/spm_files/test5');
 %% make new MEEG object 
- D = clone(D,['/home/frederik/data/EEG_rest/spm_data/spm_meeg_' fold(i).name(1:4) '_EO_EC'],[64 size(EEG.data(:,end-1600:end),2) 2],0)
+ D = clone(D,['/Users/fvdsteen/data/EEG_rest/spm_data/spm_meeg_' fold(i).name(1:4) '_EO_EC'],[64 size(EEG.data(:,end-1600:end),2) 2],0)
 %% put the EEG data in the MEEG object  
 D(:,:,1) = EEG.data(:,end-1600:end);
 D(:,:,2) =EEG2.data(:,1:1601);
@@ -35,8 +35,4 @@ if isfield(EEG,'badchans')
 end
 % D.save
 STUDY = []; CURRENTSTUDY = 0; ALLEEG = []; EEG=[]; CURRENTSET=[];
-% D(:,:,2) = no(1:64,:,i);
-% 
-% eeglab redraw
-%%
 end
